8a23addd00e2c9a0d40229b30c7f71ee6bf21f8d
JJ <wkdwndud753@naver.com>
2019-03-07 14:57:25 +0900

    Chapter 10 Class

---
+++ object_oriented.py
@@ -0,0 +1,14 @@
+from user import User
+
+user1 = User()
+user2 = User()
+
+user1.name = "Young"
+user1.email = "young@codeit.kr"
+user1.password = "123456"
+
+user2.name = "Yoonsoo"
+user2.email = "yoonsoo@codeit.kr"
+user2.password = "abcdef"
+
+user1.say_my_name()
---
+++ user.py
@@ -0,0 +1,3 @@
+class User:
+    def say_my_name(self):
+        print("My name is " + self.name)
